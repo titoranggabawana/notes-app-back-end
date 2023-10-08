0a994bcb886dad756b19057dbaa68530bf7744c8
titorangga <titoranggabawana123@gmail.com>
2023-10-08 12:39:04 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,21 @@
+const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+        options: {
+          cors: {
+            origin: ['*'],
+          },
+        },
+      },
+    {
+     method: 'GET',
+     path: '/notes',
+     handler: getAllNotesHandler,
+     },
+  ];
+   
+  module.exports = routes;
